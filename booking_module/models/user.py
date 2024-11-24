30e7ebe5d4ad50c70324b695187dd1cb0216a009
oliverostojic <oliver.ostojic.swe@gmail.com>
2024-11-24 14:46:07 -0500

    Removed address and insurance from user model.

--- booking_module/models/user.py
+++ booking_module/models/user.py
@@ -11,21 +11,6 @@ class AccountStatus(str, Enum):
     SUSPENDED = "suspended"
 
 
-class Insurance(BaseModel):
-    group_id: str
-    member_id: str
-    insurance_company_name: str
-
-
-class Address(BaseModel):
-    street_address: str
-    apartment_unit: Optional[str] = None
-    city: str
-    state: str
-    postal_code: str
-    country: str = Field(default="United States of America")
-
-
 class FullName(BaseModel):
     first: str
     last: str
@@ -38,7 +23,6 @@ class User(BaseModel):
     hashed_password: str
     appointments: Optional[List[Appointment]] = []
     account_status: AccountStatus = Field(default=AccountStatus.ACTIVE)
-    insurance: Insurance
 
     def full_name(self) -> str:
         return f"{self.name['first']} {self.name['last']}"
